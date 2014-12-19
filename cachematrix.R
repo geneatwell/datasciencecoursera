81b314eb2410b8e29fa18b8dbda6fcb0f48ea133
Gene Atwell <geneatwell@gmail.com>
2014-12-18 22:38:41 -0500

    Cached Matrix
    Two functions to cache an inverted matrix and save computation time.

--- cachematrix.R
+++ cachematrix.R
@@ -24,15 +24,15 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-##The function below  retrieves from cache or computes the inverse of a matrix entered into makeCacheMatrix()
-##above utilizing the list created by makeCacheMatrix() and the information stored as a result. If the inverse
-##has already been calculated (and the matrix has not changed), then cachesolve() retrieves the inverse from
-##the cache, supposedly saving time.  Timing tests have shown that running the function twice on the same
-##matrix (1000X1000) reduces the amount of time required for processing. 
+## The function below retrieves from cache or computes the inverse of a matrix entered into makeCacheMatrix()
+## above utilizing the list created by makeCacheMatrix() and the information stored as a result. If the inverse
+## has already been calculated (and the matrix has not changed), then cacheSolve() retrieves 'inverse' from
+## the cache, supposedly saving time.  Timing tests have shown that running the function twice on the same
+## matrix (1000X1000) reduces the amount of time required for processing once the inverse is cached. 
 
 
-##Initial timings: user  system elapsed   ##Second time through: user  system elapsed
-##                0.143   0.001   0.068   ##                        0       0       0
+## Initial timings: user  system elapsed   ##Second time through: user  system elapsed
+##                 0.143   0.001   0.068   ##                        0       0       0
 
 
 
@@ -40,13 +40,13 @@ cacheSolve <- function(x, ...) {
         
         inverse <- x$getinverse()  ## inverted matrix is loaded from above
         if(!is.null(inverse)) {  ## if that matrix is empty, the flow bypasses the next two lines
-                message("Data already cached!")  ##this line is printed if the 'inverse' was previously computed.
-                return(inverse)                 ##and the already computed 'inverse' is used, thus saving time.      
+                message("Data already cached!")  ## this line is printed if the 'inverse' was previously computed.
+                return(inverse)                 ## and the already computed 'inverse' is used, thus saving time.      
         }
         
-        data <- x$get()              ##this part of the function computes 'inverse' if there was no cached matrix
+        data <- x$get()              ## this part of the function computes 'inverse' if there was no cached matrix
         inverse <- solve(data, ...)
         x$setinverse(inverse)
         
-        inverse                      ##and this is the returned, newly calculated, inverted matrix.
+        inverse                      ## and this is the returned, newly calculated, inverted matrix.
 }
